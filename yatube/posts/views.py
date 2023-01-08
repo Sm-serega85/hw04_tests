b834d023bdec10256f34be6ea19eb7eb9b2e1642
Сергей Смирнов <sm-serega85@yandex.ru>
2023-01-08 18:07:59 +0300

    sprint5_20230108

---
+++ yatube/posts/views.py
@@ -0,0 +1,74 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from posts.utils import get_page_paginator
+
+from .forms import PostForm
+from .models import Group, Post, User
+
+
+def index(request):
+    post_list = Post.objects.all()
+    context = {
+        'page_obj': get_page_paginator(request, post_list)
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.all()
+    context = {
+        'group': group,
+        'page_obj': get_page_paginator(request, post_list)
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    author_posts = author.posts.all()
+    context = {
+        'author': author,
+        'page_obj': get_page_paginator(request, author_posts)
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', post.author)
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    is_edit = True
+    post = get_object_or_404(Post, pk=post_id)
+    if request.user != post.author:
+        return redirect('posts:post_detail', post_id=post.id)
+
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post.id)
+
+    context = {
+        'form': form,
+        'post': post,
+        'is_edit': is_edit
+    }
+    return render(request, 'posts/create_post.html', context)
